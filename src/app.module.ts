c0826e7ffe031b9c4a7606e95947ba8c72cf3164
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-19 11:27:33 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
