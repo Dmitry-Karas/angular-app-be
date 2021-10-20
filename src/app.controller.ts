ab2de1e8e8c3f81fd86a4cc0f4b73d4eb44746d9
Dmitry Karas <dmitrythekaras@gmail.com>
2021-10-20 19:10:40 +0300

    refactor: delete unnecessary code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,4 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/guards/local-auth.guard';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly authService: AuthService,
-  ) {}
-
-  // @UseGuards(LocalAuthGuard)
-  // @Post('auth/login')
-  // async login(@Request() req) {
-  //   return this.authService.login(req.user);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Get('profile')
-  // getProfile(@Request() req) {
-  //   return req.user;
-  // }
-}
+export class AppController {}
